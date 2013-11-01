c1a333e3284ac2da21ffb7a7ba2a59b0ad2d4946
tkoane <tkoane@gmail.com>
2013-10-31 22:10:37 -0400

    added data files

---
+++ hw3_code_dzbarsky.py
@@ -0,0 +1,17 @@
+# David Zbarsky: dzbarsky@wharton.upenn.edu
+# Yaou Wang: yaouwang@wharton.upenn.edu
+
+from nltk.corpus import PlaintextCorpusReader
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem.porter import PorterStemmer
+import math
+import string
+import random
+import fileinput
+import os
+import itertools
+import subprocess
+
+'''
+homework 3 by David Zbarsky and Yaou Wang
+'''
