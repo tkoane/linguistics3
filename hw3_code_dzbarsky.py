1d5354daa6917b8b70df45a5f3fe61ba4e30700d
tkoane <tkoane@gmail.com>
2013-10-31 22:40:20 -0400

    first couple of basic functions

--- hw3_code_dzbarsky.py
+++ hw3_code_dzbarsky.py
@@ -15,3 +15,57 @@ import subprocess
 '''
 homework 3 by David Zbarsky and Yaou Wang
 '''
+
+#from hw1: functions for translating files/directory into tokens
+def get_all_files(directory):
+    # We assume that a filename with a . is always a file rather than a directory
+    # IF we were passed a file, just return that file.  This simplifies representing documents because they need to handle single files.
+    if directory.find('.') < 0:
+        return PlaintextCorpusReader(directory, '.*').fileids()
+    #if directory is a file return the file in a list
+    return [directory]
+
+def load_file_sentences(filepath):
+    index = filepath.rfind('/')
+    dir = filepath[:index]
+    filepath = filepath[index + 1:]
+    return sent_tokenize(PlaintextCorpusReader(dir, filepath).raw().lower())
+
+def load_file_tokens(filepath):
+    tokens = []
+    for sentence in load_file_sentences(filepath):
+        tokens.extend(word_tokenize(sentence))
+    return tokens
+
+def load_collection_tokens(directory):
+    tokens = []
+    for file in get_all_files(directory):
+        tokens.extend(load_file_tokens(directory + '/' + file))
+    return tokens
+#end of functions from hw1
+
+#returns a list of all words occurring >= 5 times in the directory
+def extract_top_words(directory):
+    tokens = load_collection_tokens(directory)
+    freq = dict()
+    top_words = []
+    for token in tokens:
+        if token in freq.keys():
+            freq[token] += 1
+        else:
+            freq[token] = 1
+    for word in freq.keys():
+        if freq[word] >= 5:
+            top_words.append(word)
+    return top_words
+
+def unigram_map_entry(filename, top_words):
+    tokens = load_file_tokens(filename)
+    output_list = [0] * len(top_words)
+    for token in tokens:
+        try:
+            i = top_words.index(token)
+            output_list[i] += 1
+        except Error:
+            pass
+    return output_list
