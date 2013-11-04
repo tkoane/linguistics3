c0f42aeb01ec25ddfad68dafefc5273fe2dd2541
tkoane <tkoane@gmail.com>
2013-11-04 16:34:00 -0500

    testing

--- hw3_code_dzbarsky.py
+++ hw3_code_dzbarsky.py
@@ -242,6 +242,8 @@ def map_verb_dependencies(xml_filename, dependency_list):
                 pass
     return array
 
+
+
 def main():
     #top_words = extract_top_words('/home1/c/cis530/hw3/data')
     #print unigram_map_entry('/home1/c/cis530/hw3/data/6285515.txt', top_words)
@@ -258,7 +260,9 @@ def main():
     #os.system('java -cp stanford-corenlp-2012-07-09.jar:stanford-corenlp-2012-07-06-models.jar:xom.jar:joda-time.jar -Xmx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse -filelist test_datafilelist.txt -outputDirectory test_data_result')
     #print extract_named_entities('data_result/71964.txt.xml')
     #print extract_adjectives('data_result')
-    print map_verb_dependencies('data_result/71964.txt.xml', extract_verb_dependencies('data_result'))
+    dictionary = extract_verb_dependencies('data_result')
+    print dictionary
+    print map_verb_dependencies('data_result/71964.txt.xml', dictionary)
     #print map_adjectives('data_result/71964.txt.xml', ['big', 'small', 'public'])
 
 if __name__ == "__main__":
