666c07cf423d0aedc2d59389a562aaadc8bc6768
tkoane <tkoane@gmail.com>
2013-11-03 23:00:58 -0500

    testing part 5.2

--- hw3_code_dzbarsky.py
+++ hw3_code_dzbarsky.py
@@ -258,7 +258,7 @@ def main():
     #os.system('java -cp stanford-corenlp-2012-07-09.jar:stanford-corenlp-2012-07-06-models.jar:xom.jar:joda-time.jar -Xmx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse -filelist test_datafilelist.txt -outputDirectory test_data_result')
     #print extract_named_entities('data_result/71964.txt.xml')
     #print extract_adjectives('data_result')
-    print extract_verb_dependencies('data_result')
+    print map_verb_dependencies('data/result/71964.txt.xml', extract_verb_dependencies('data_result'))
     #print map_adjectives('data_result/71964.txt.xml', ['big', 'small', 'public'])
 
 if __name__ == "__main__":
